d18b7497d142682b6f01876a3e8f00e315158c8c
Herick Teixeira <herickratai@gmail.com>
2024-05-20 14:02:08 -0300

    initial commit

---
+++ src/main/java/com/itemshexagonalarch/ItemsHexagonalArchApplication.java
@@ -0,0 +1,13 @@
+package com.itemshexagonalarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsHexagonalArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsHexagonalArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itemshexagonalarch/ItemsHexagonalArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itemshexagonalarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsHexagonalArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
